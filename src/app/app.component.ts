54346795c3b88771d30ac0e3593c2aac435e781b
Omar Boninsegna <o.boninsegna@reply.com>
2024-11-18 23:35:34 +0100

    And so it begins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import options from './options';
+import { NgParticlesModule } from 'ng-particles';
+import { Engine } from 'tsparticles-engine';
+import { loadSlim } from 'tsparticles-slim';
+import _particlesOptions from './particles-options';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [NgParticlesModule],
 })
 export class AppComponent {
   title = 'wheelgood';
+
+  selectedOption: string;
+  isSpinning: boolean;
+  spinningOption: string;
+
+  timeout;
+  wheelSpinningTime;
+
+  particlesOptions = _particlesOptions;
+
+  stopWheel() {
+    this.isSpinning = false;
+    this.selectedOption = this.getRandomOption();
+    clearTimeout(this.timeout);
+    clearTimeout(this.wheelSpinningTime);
+  }
+
+  getRandomOption(): string {
+    const rd = Math.round(Math.random() * (options.length - 1));
+    return options[rd];
+  }
+
+  startWheel() {
+    this.isSpinning = true;
+    this.timeout = setTimeout(() => {
+      this.wheelSpinning();
+      this.startWheel();
+    }, 10);
+  }
+
+  wheelSpinning() {
+    this.spinningOption = this.getRandomOption();
+  }
+
+  @HostListener('mousedown')
+  clickEvent() {
+    if (!this.isSpinning) {
+      this.startWheel();
+      this.wheelSpinningTime = setTimeout(() => {
+        this.stopWheel();
+      }, Math.floor(Math.random() * 1000) + 500);
+    }
+  }
+
+  async particlesInit(engine: Engine): Promise<void> {
+    await loadSlim(engine);
+  }
 }
---
+++ src/app/particles-options.js
@@ -0,0 +1,72 @@
+export default {
+  background: {},
+  particles: {
+    number: {
+      value: 388,
+      density: {
+        enable: false,
+        value_area: 1341.5509907748635,
+      },
+    },
+    color: {
+      value: '#ffffff',
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000',
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: 'img/github.svg',
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.33933348590187723,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 10,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+      distance: 500,
+      color: '#ffffff',
+      opacity: 0.4,
+      width: 2,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: 'bottom',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  retina_detect: true,
+};
